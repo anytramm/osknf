bdf6599357dea541adff4a3c52022c651283b56d
anytramm <z.martyna96@gmail.com>
2020-07-21 11:33:32 +0200

    Updates

---
+++ precache-manifest.91ef8c4754406967075beca7c63eacb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a94a94a73ce5bcbb99e326cad9f7c4",
+    "url": "/osknf/index.html"
+  },
+  {
+    "revision": "9b4a36d15a9c6405ff5a",
+    "url": "/osknf/static/css/main.da40ba64.chunk.css"
+  },
+  {
+    "revision": "ecc41943f668881aea38",
+    "url": "/osknf/static/js/2.c1f42c4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/osknf/static/js/2.c1f42c4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4a36d15a9c6405ff5a",
+    "url": "/osknf/static/js/main.619d31ff.chunk.js"
+  },
+  {
+    "revision": "7856e8a738693ec474d0",
+    "url": "/osknf/static/js/runtime-main.212b0d42.js"
+  },
+  {
+    "revision": "f595c5452e4202dfcbcb118485af4b8d",
+    "url": "/osknf/static/media/person.f595c545.jpg"
+  }
+]);
